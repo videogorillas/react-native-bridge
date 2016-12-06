4c74b64e91a99224b6fceed6561acf277c22e838
day <matilda.tereshkova@gmail.com>
2016-12-06 19:51:57 +0200

    initial

---
+++ src/com/vg/js/bridge/reactnative/Text.java
@@ -0,0 +1,10 @@
+package com.vg.js.bridge.reactnative;
+
+import org.stjs.bridge.react.internal.ReactComponent;
+import org.stjs.bridge.react.internal.ReactElement;
+import org.stjs.javascript.annotation.STJSBridge;
+
+@STJSBridge
+public class Text<C extends ReactElement<?>> extends ReactComponent<C> {
+
+}
